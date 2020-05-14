961be4aea8204b3e2104bff201c828606aaf2587
duc <duc@gmail.com>
2020-05-15 00:22:34 +0700

    fix PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-var http = require("http").createServer(app).listen(3000); // http://192.168.0.88:3000
+var http = require("http").createServer(app).listen(process.env.PORT || 3000); // http://192.168.0.88:3000
 var io = require("socket.io")(http);
 var fs = require("fs");
 
