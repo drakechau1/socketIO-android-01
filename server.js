2b387fec7026cc8ceaf5808859c93d8ce3e9ef2f
duc <duc@gmail.com>
2020-05-15 00:13:22 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var http = require("http").createServer(app).listen(3000); // http://192.168.0.88:3000
+var io = require("socket.io")(http);
+var fs = require("fs");
+
+io.sockets.on("connection", function(){
+    console.log("connected");
+});
+
+app.get("/", function(req, res){
+    res.render("home");
+});
